fb064486d9918d7c3b71677bb50fd6ef11d60b9f
Iuri Dias <iuridias.mw@gmail.com>
2022-03-29 20:49:16 -0300

    projeto-final

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
-  
-  constructor(elemento) {
-    this._elemento = elemento;
+class NegociacoesView extends View {
+
+  constructor(elemento){
+    super(elemento);
   }
   
-  _template(model) {
+  template(model) {
     return `
     <table class="table table-hover table-bordered">
         <thead>
@@ -40,7 +40,4 @@ class NegociacoesView {
     `
   }
 
-  update(model) {
-    this._elemento.innerHTML = this._template(model);
-  }
 }
